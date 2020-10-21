d2541e97f96380e9f671db6d9c383edf3841dca8
arthursvpb <arthurandroid15@gmail.com>
2020-10-21 18:21:15 -0300

    connecting to db with mongoose

--- index.js
+++ index.js
@@ -1,13 +1,31 @@
+// Dependencies
 const express = require("express");
 const bodyParser = require("body-parser");
 
+// Routes
 const users = require("./routes/users.router");
 
-const port = 3333;
+// Starting express
 const app = express();
 
+// Connect to DB
+const mongoose = require("mongoose");
+mongoose.connect("mongodb://localhost:27017/crud-app", {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+const db = mongoose.connection;
+db.on("error", console.error.bind(console, "connection error -"));
+
+// Body Parser
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+
+// First route
 app.use("/users", users);
 
+// Server
+const port = 3333;
 app.listen(port, () => {
   console.log(`App listening on port ${port}`);
 });
