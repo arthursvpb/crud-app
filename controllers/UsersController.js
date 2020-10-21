72a69daf8239f3e4eed3f098f1b256f0223c5f42
arthursvpb <arthurandroid15@gmail.com>
2020-10-21 19:43:10 -0300

    read ok

--- controllers/UsersController.js
+++ controllers/UsersController.js
@@ -4,8 +4,18 @@ exports.create = (req, res) => {
   const user = new User({ login: req.body.login, password: req.body.password });
   user.save((err) => {
     if (err) {
-      console.log("Error creating user");
+      console.warn("Error creating user");
     }
     res.send(`Register successful: ${user}`);
+  });
+};
+
+exports.read = (req, res) => {
+  // res.send(req.params.id);
+  User.findById(req.params.id, (err, user) => {
+    if (err) {
+      console.warn(`Error reading user ${user}`);
+    }
+    res.send(`Showing user: ${user}`);
   });
 };
--- routes/users.router.js
+++ routes/users.router.js
@@ -4,6 +4,7 @@ const router = express.Router();
 const UsersController = require("../controllers/UsersController");
 
 router.post("/create", UsersController.create);
+router.get("/read/:id", UsersController.read);
 // router.get("/test", UsersController.test);
 
 module.exports = router;
